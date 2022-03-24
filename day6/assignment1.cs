c89f030003e00d1a98a5e97bc1da5ef273ffc257
Kaijing Wang <wangk35@miamioh.edu>
2022-03-23 23:17:54 -0400

    Day 6 C# assignment

---
+++ day6/assignment1.cs
@@ -0,0 +1,303 @@
+﻿
+
+
+//Console.Write("What's your favorite color? ");
+//string color = Console.ReadLine();
+//Console.Write("What's your astrology sign? ");
+//string sign = Console.ReadLine();
+//Console.Write("What's your street address number? ");
+//string num = Console.ReadLine();
+
+//Console.WriteLine($"Your hacker name is {color + sign + num}");
+
+//---------------------------------------------------------//
+
+//Practice number sizes and ranges
+//1.Create a console application project named /02UnderstandingTypes/ that outputs
+//the number of bytes in memory that each of the following number types uses,
+//and the minimum and maximum values they can have:
+//sbyte, byte, short, ushort, int, uint, long, ulong, float, double, and decimal.
+
+//Console.WriteLine($"sbyte max = {sbyte.MaxValue}, min = {sbyte.MinValue}");
+
+//Console.WriteLine($"byte max = {byte.MaxValue}, min = {byte.MinValue}");
+
+//Console.WriteLine($"short max = {short.MaxValue}, min = {short.MinValue}");
+
+//Console.WriteLine($"ushort max = {ushort.MaxValue}, min = {ushort.MinValue}");
+
+//Console.WriteLine($"int max = {int.MaxValue}, min = {int.MinValue}");
+
+//Console.WriteLine($"uint max = {uint.MaxValue}, min = {uint.MinValue}");
+
+//Console.WriteLine($"long max = {long.MaxValue}, min = {long.MinValue}");
+
+//Console.WriteLine($"ulong max = {ulong.MaxValue}, min = {ulong.MinValue}");
+
+//Console.WriteLine($"float max = {float.MaxValue}, min = {float.MinValue}");
+
+//Console.WriteLine($"double max = {double.MaxValue}, min = {double.MinValue}");
+
+//Console.WriteLine($"decimal max = {decimal.MaxValue}, min = {decimal.MinValue}");
+
+
+
+//---------------------------------------------------------//
+
+//Composite Formatting to learn how to align text in a console application
+//2.
+//Write program to enter an integer number of centuries and convert it to 
+//years, days, hours, minutes, seconds, milliseconds, microseconds, nanoseconds.
+//Use an appropriate data type for every data conversion. Beware of overflows!
+//Input: 1
+//Output: 1 centuries = 100 years = 36524 days = 876576 hours = 52594560 minutes
+//= 3155673600 seconds = 3155673600000 milliseconds = 3155673600000000
+//microseconds = 3155673600000000000 nanoseconds
+//Input: 5
+//Output: 5 centuries = 500 years = 182621 days = 4382904 hours = 262974240
+//minutes = 15778454400 seconds = 15778454400000 milliseconds = 15778454400000000
+//microseconds = 15778454400000000000 nanoseconds
+
+//string s = Console.ReadLine();
+//UInt64 century = Convert.ToUInt64(s);
+//Console.WriteLine($"{century} century = {century * 100} years = {century * 100 * 365} days = " +
+//    $"{century * 100 * 365 * 24} hours = {century * 100 * 365 * 24 * 60} minutes = " +
+//    $"{century * 100 * 365 * 24 * 60 * 60} seconds = {century * 100 * 365 * 24 * 60 * 60 * 1000} milliseconds = " +
+//    $" {century * 100 * 365 * 24 * 60 * 60 * 1000 * 1000} microseconds = " +
+//    $" {century * 100 * 365 * 24 * 60 * 60 * 1000 * 1000 * 1000} nanoseconds");
+
+
+
+//---------------------------------------------------------//
+
+//Switch Expression ( slightly different from old switch )
+//string s = Console.ReadLine();
+
+//string re = s switch { 
+//    "abc" => "abc",
+//    "123" => "number",
+//    _     => "anything else"
+//};
+
+//Console.WriteLine(re);  
+
+
+
+//---------------------------------------------------------//
+//Practice loops and operators
+//1.FizzBuzz is a group word game for children to teach them about division. 
+//Players take turns to count incrementally, 
+//replacing any number divisible by three with the word /fizz/, 
+//any number divisible by five with the word /buzz/,
+//and any number divisible by both with /fizzbuzz/. 
+//Create a console application in Chapter03 named Exercise03 that outputs a
+//simulated FizzBuzz gamecounting up to 100.
+
+//string s = "";
+//for (int i = 1; i <= 100; i++)
+//{
+//    if(i % 3 == 0 && i % 5 == 0)
+//    {
+//        s += "FizzBuzz ";  
+//    }else if(i % 3 == 0)
+//    {
+//        s += "fizz ";
+//    }else if(i % 5 == 0)
+//    {
+//        s += "buzz ";
+//    }
+//    else
+//    {
+//        s += i + " ";
+//    }
+//}
+
+//Console.WriteLine(s);
+
+
+
+
+//infinite loop since byte max at 255,never reach 500
+//int max = 500;
+//for (byte i = 0; i < max; i++) {
+//    Console.WriteLine(i); 
+//}
+
+
+
+
+
+//---------------------------------------------------------//
+//Write a program that generates a random number between 1 and 3 and 
+//asks the user to guess what the number is. 
+//Tell the user if they guess low, high, or get the correct answer. 
+//Also, tell the user if their answer is outside of the range of numbers that are valid guesses 
+//(less than 1 or more than 3). 
+
+
+//int correctNumber = new Random().Next(3) + 1;
+
+//bool correct = false;
+//while (!correct)
+//{
+//    int guessedNumber = int.Parse(Console.ReadLine());
+
+
+
+//    if (guessedNumber == correctNumber)
+//    {
+//        correct = true;
+//        Console.WriteLine("You got it right");
+//    } else if (guessedNumber < 1 || guessedNumber > 3)
+//    {
+//        Console.WriteLine("Not even close, try again");
+//    }else if(guessedNumber > correctNumber)
+//    {
+//        Console.WriteLine("too high");
+//    }
+//    else
+//    {
+//        Console.WriteLine("too low");
+//    }
+
+//}
+
+
+
+
+//---------------------------------------------------------//
+
+//2.
+//Print - a - Pyramid.
+//Like the star pattern examples that we saw earlier, create a program that 
+//will print the following pattern: If you find yourself getting stuck, try recreating the two 
+//examples that we just talked about in this chapter first. They’re simpler, and you can 
+//compare your results with the code included above. 
+//This can actually be a pretty challenging problem, so here is a hint to get you going. I used 
+//three total loops. One big one contains two smaller loops. The bigger loop goes from line 
+//to line. The first of the two inner loops prints the correct number of spaces, while the 
+//second inner loop prints out the correct number of stars. 
+
+
+//for(int i = 1; i <= 9; i += 2)
+//{
+//    for(int j = 1; j <= (9 - i)/2; j++)
+//    {
+//        Console.Write(" ");
+//    }
+
+
+//    for(int j = 1; j <= i; j++)
+//    {
+//        Console.Write("*");
+//    }
+
+//    for (int j = 1; j <= (9 - i) / 2; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+//---------------------------------------------------------//
+
+
+//4.
+//Write a simple program that defines a variable representing a birth date and calculates 
+//how many days old the person with that birth date is currently.
+//For extra credit, output the date of their next 10,000 day (about 27 years) anniversary.
+//Note: once you figure out their age in days, you can calculate the days until the next 
+//anniversary using
+//int daysToNextAnniversary = 10000 - (days % 10000);
+
+//DateTime d = DateTime.UtcNow;
+
+//Console.WriteLine("What year are you born?");
+//int year  = int.Parse(Console.ReadLine());
+//Console.WriteLine("What month are you born?");
+//int mon = int.Parse(Console.ReadLine()); 
+//Console.WriteLine("What day are you born?");
+//int day = int.Parse(Console.ReadLine());
+
+//DateTime birthday = new DateTime(year,mon,day);
+
+//Console.WriteLine($"You lived :{(d - birthday).Days} days");
+//int daysToNextAnniversary = 10000 - ((d - birthday).Days % 10000);
+//Console.WriteLine($"Next 10000 day anni is {daysToNextAnniversary} days from now");
+
+
+
+
+
+//---------------------------------------------------------//
+
+//5.
+//Write a program that greets the user using the appropriate greeting for the time of day.
+//Use only
+//if, not else or switch, statements to do so. Be sure to include the following 
+//greetings:
+//"Good Morning"
+//"Good Afternoon"
+//"Good Evening"
+//"Good Night"
+//It 's up to you which times should serve as the starting and ending ranges for each of the 
+//greetings.If you need a refresher on how to get the current time, see 
+//DateTime Formatting
+//. When testing your program, you'll probably want to use a
+//DateTime variable you define, rather than the current time. Once you're confident the program works 
+//correctly, you can substitute
+//DateTime.Now for your variable (or keep your variable and just 
+//Assign DateTime.Now as its value, which is often a better approach).
+
+//DateTime time = new DateTime(2022, 3, 23, 22, 0, 0);
+
+//DateTime time = DateTime.Now;
+//Console.WriteLine(time.ToString());
+
+//if(time.Hour > 4 && time.Hour < 12)
+//{
+//    Console.WriteLine("Good Morning");
+//}else if(time.Hour > 11 && time.Hour < 17)
+//{
+//    Console.WriteLine("Good Afternoon");
+
+//}else if (time.Hour > 16 && time.Hour < 22)
+//{
+//    Console.WriteLine("Good Evening");
+//}
+//else
+//{
+//    Console.WriteLine("Good Night");
+//}
+
+
+
+
+//6.
+//Write a program that prints the result of counting up to 24 using four different increments.
+//First, count by 1s, then by 2s, by 3s, and finally by 4s.
+//Use nested for loops with your outer loop counting from 1 to 4. You inner loop should 
+//count from 0 to 24, but increase the value of its /loop control variable/ by the value of the
+// / loop control variable/ from the outer loop.
+// This means the incrementing in the /afterthought/ expression will be based on a variable.
+//Your output should look something like this:
+//0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
+//0,2,4,6,8,10,12,14,16,18,20,22,24
+//0,3,6,9,12,15,18,21,24
+//0,4,8,12,16,20,24
+
+for (int i = 1; i <= 4; i++)
+{
+    for (int j = 0; j <= 24; j += i)
+    {
+        Console.Write(j + ", ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
---
+++ day6/assignment2.cs
@@ -0,0 +1,347 @@
+﻿// Declare a single-dimensional array of 5 integers.
+//int[] arr = new int[5];
+//arr[0] = 0;
+//arr[1] = 1;
+
+//var arr2 = (int[])arr.Clone();
+
+//arr2[0] = 2;
+
+//int[] arr3 = new int[5];
+//arr.CopyTo(arr3, 0);
+
+//arr3[0] = 2;
+//foreach (int i in arr)
+//{
+//    Console.Write(i + " ");
+//}
+//Console.WriteLine();
+
+//foreach (int i in arr2)
+//{
+//    Console.Write(i + " ");
+
+//}
+
+
+//--------------------------------------------------------------------//
+//Practice Arrays
+//1.
+//Copying an Array
+//Write code to create a copy of an array. First, start by creating an initial array. (You can use 
+//whatever type of data you want.) Let’s start with 10 items. Declare an array variable and 
+//assign it a new array with 10 items in it.Use the things we’ve discussed to put some values in the array. 
+//Now create a second array variable. Give it a new array with the same length as the first. 
+//Instead of using a number for this length, use the Length
+//property to get the size of the original array.
+//Use a loop to read values from the original array and place them in the new array.
+//Also print out the contents of both arrays, to be sure everything copied correctly.
+
+
+//int[] arr = new int[10];
+
+//for(int i = 0; i < arr.Length; i++)
+//{
+//    arr[i] = i;
+//}
+
+//int[] arr2 = new int[arr.Length];
+
+//for (int i = 0; i < arr2.Length; i++)
+//{
+//    arr2[i] = arr[i];
+//}
+
+//for(int i = 0; i < arr.Length; i++)
+//{
+//    Console.Write(arr[i]);
+//}
+//Console.WriteLine();
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    Console.Write(arr2[i]);
+//}
+
+
+
+//--------------------------------------------------------------------//
+//2.
+//Write a simple program that lets the user manage a list of elements.
+//It can be a grocery list, "to do" list, etc. Refer to 
+//Looping Based on a Logical Expression
+// if necessary to see how to 
+//implement an infinite loop. Each time through the loop, ask the user to perform an 
+//operation, and then show the current contents of their list. The operations available should 
+//be Add, Remove, and Clear. The syntax should be as follows:
+//+ some item
+//- some item
+//--
+//Your program should read in the user's input and determine if it begins with a  “+” or “-“ or 
+//if it is simply  “—-“ . In the first two cases, your program should add or remove the string 
+//given ("some item" in the example). If the user enters just “—“ then the program should 
+//clear the current list. Your program can start each iteration through its loop with the 
+//following instruction:
+//Console.WriteLine("Enter command (+ item, - item, or -- to clear)):");
+
+//string result = "";
+
+//while (true)
+//{
+//    Console.WriteLine("Enter command (+ item, - item, or -- to clear)):");
+
+//    string s = Console.ReadLine();
+//    string op = s.Substring(0, 2);
+//    string item = s.Trim().Substring(2);
+
+//    if (op.Equals("--"))
+//    {
+//        //clear
+//        result = "";
+//        Console.WriteLine("clear");
+//    }else if(op[0] == '-')
+//    {
+//        if (result.Contains(item))
+//        {
+//            //remove
+//            //Console.WriteLine("remove");
+//            for (int i = 0; i < result.Length - item.Length + 1; i++)
+//            {
+//                if(result.Substring(i, item.Length).Equals(item))
+//                {
+//                    result = result.Substring(0,i - 2) + result.Substring(i + item.Length);
+//                    break;  
+//                }
+//            }
+//        }
+
+
+//    }
+//    else if(op[0] == '+')
+//    {
+//        //add
+//        //Console.WriteLine("add");
+//        result += ", " + item;
+
+//    }
+//    else
+//    {
+//        Console.WriteLine("Incorrect Command");
+//    }
+
+//    Console.WriteLine("current list" + result);
+//}
+
+
+
+
+
+//--------------------------------------------------------------------//
+
+//3.
+//Write a method that calculates 
+//all prime numbers in given range
+// and returns them as array 
+//of integers
+
+//int[] arr = FindPrimesInRange(0, 20);
+
+//foreach(var item in arr)
+//{
+//   Console.WriteLine(item);
+//}
+
+//static int[] FindPrimesInRange(int startNum, int endNum) {
+
+//    List<int> primes = new List<int>();
+//    for(int i = startNum; i <= endNum; i++)
+//    {
+//        if(isPrime(i))
+//            primes.Add(i);
+//    }
+
+//    return primes.ToArray();
+
+//}
+
+//static bool isPrime(int num)
+//{
+//    for(int i = 2; i <= Math.Sqrt(num); i++)
+//    {
+//        if (num % i == 0)
+//            return false;
+//    }
+//    return true;
+//}
+
+
+
+
+
+//--------------------------------------------------------------------//
+
+//4.
+//Write a program to read an array of n integers
+// (space separated on a single line) and an 
+//integer k, rotate the array right k times
+// and sum the obtained arrays after each rotation as 
+//shown below.
+//After r rotations the element at position I goes to position (I + r) % n.
+//The sum[] array can be calculated by two nested loops:  
+//for r = 1... k; for I = 0... n - 1.
+
+//Input         Output          Comments
+//3 2 4 - 1      3 2 5 6        rotated1[] = -1  3  2  4
+//2                             rotated2[] = 4 - 1  3  2
+//                              sum[]      = 3  2  5  6
+
+//1 2 3 4 5     12 10 8 6 9     rotated1[] = 5  1  2  3  4
+//3                             rotated2[] = 4  5  1  2  3
+//                              rotated3[] = 3  4  5  1  2
+//                              sum[]      = 12 10  8  6  9
+
+
+//string s = Console.ReadLine();
+//int k = int.Parse(Console.ReadLine());
+
+//string[] arr = s.Split(' ');
+//int[] nums = new int[arr.Length];
+//int[] sum = new int[arr.Length];
+
+//for(int i = 0; i < arr.Length; i++)
+//{
+//    nums[i] = int.Parse(arr[i]);
+//}
+
+
+//for(int i = 0; i < k; i++)
+//{
+//    int last = nums[nums.Length - 1];
+//    //rotate once
+//    for(int j = nums.Length - 1;  j > 0;  j--)
+//    {
+//        nums[j] = nums[j - 1];
+//    }
+//    nums[0] = last;
+
+
+//    //add to sum
+//    for(int j = 0; j < arr.Length; j++)
+//    {
+//        sum[j] += nums[j];
+//    }
+
+//}
+
+//foreach(int i in sum)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+
+
+
+
+//--------------------------------------------------------------------//
+
+//5
+//.
+//Write a program that finds the 
+//longest sequence of equal elements
+// in an array of integers. 
+//If several longest sequences exist, print the leftmost one.
+//Input                          Output
+//2 1 1 2 3 3 2 2 2 1            2 2 2
+//1 1 1 2 3 1 3 3                1 1 1
+//4 4 4 4                        4 4 4 4
+//0 1 1 5 2 2 6 3 3              1 1
+
+//while (true)
+//{
+
+//    string s = Console.ReadLine();
+//    string[] arr = s.Trim().Split(' ');
+//    int[] nums = new int[arr.Length];
+
+//    for (int i = 0; i < arr.Length; i++)
+//    {
+//        nums[i] = int.Parse(arr[i]);
+//    }
+
+//    string result = "";
+//    int max = 0;
+//    for (int i = 0; i < nums.Length - 1;)
+//    {
+//        int count = 0;
+//        int num = nums[i];
+//        while (i < nums.Length && num - nums[i] == 0)
+//        {
+//            count++;
+//            i++;
+//        }
+
+//        if (count > max)
+//        {
+//            result = "";
+//            for (int j = 0; j < count; j++)
+//            {
+//                result += num + " ";
+//            }
+//            max = count;
+//        }
+
+//    }
+
+//    Console.WriteLine("longest sequence: " + result);
+
+//}
+
+
+
+
+//--------------------------------------------------------------------//
+
+
+//7.
+//Write a program that finds the 
+//most frequent number
+// in a given sequence of numbers.
+// In case of multiple numbers with the same maximal frequency, print the leftmost of them
+
+while (true) { 
+string s = Console.ReadLine();
+string[] arr = s.Trim().Split(' ');
+int[] nums = new int[arr.Length];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    nums[i] = int.Parse(arr[i]);
+}
+
+
+Dictionary<int, int> map = new Dictionary<int, int>();
+
+foreach(int i in nums)
+{
+    if (map.ContainsKey(i)) 
+    { map[i] += 1; }
+    else
+    {
+        map.Add(i, 1);
+    }
+}
+
+int mostFreq = 0;
+int max = 0;
+foreach (int i in map.Keys)
+{
+    if(map[i] > max)
+    {
+        max = map[i];
+        mostFreq = i;
+    }
+}
+
+Console.WriteLine("most frequent num: " + mostFreq);
+}
